e9d320fc5560acfee4c9d2989b64f69623c27ffb
sturmanj <sturmanj@catlin.edu>
2022-11-27 18:09:29 -0800

    Started sheets and submission api

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-import sirv from 'sirv';
-import express, { Application, Request, Response } from "express";
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import type { Application } from "express";
+import sirv from "sirv";
+import express from "express";
+import compression from "compression";
+import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
 
-const app: Application = express();;
+const app: Application = express();
 
+app.use(express.json())
 app.use(compression({ threshold: 0 }));
-app.use(sirv('static', { dev }));
+app.use(sirv("static", { dev }));
 app.use(sapper.middleware());
 
 app.listen(PORT);
